6c88162b446584eb05a1049a084cf581f5ff85b1
sbk8689 <sbk8689@gmail.com>
2022-11-15 12:54:56 +0900

    [feat]문자열 계산기 구현 및 리팩토링 2-1단계

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,13 +1,25 @@
 public class StringCalculator {
     int add(String text){
-        if(text == null || text.isEmpty()){
+        if(isBlank(text)){
             return 0;
         }
-            String[] values = text.split(",");
-            int sum = 0;
-            for (String value: values) {
-                sum += Integer.parseInt(value);
-            }
-            return sum;
+        return sum(split(text));
+    }
+
+    private boolean isBlank(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private String[] split(String text) {
+        String[] values = text.split(",");
+        return values;
+    }
+
+    private int sum(String[] values) {
+        int sum = 0;
+        for (String value: values) {
+            sum += Integer.parseInt(value);
+        }
+        return sum;
     }
 }
